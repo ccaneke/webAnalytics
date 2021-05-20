42b089d15b007c9344a13205154fd26e725bcba0
Chukwuemezie Christopher Aneke <chukwuemezie.aneke.15@ucl.ac.uk>
2021-05-20 15:41:59 +0200

    write main function to see how code works

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/ccaneke/webAnalytics/webAnalyzer"
+)
+
+func main() {
+	const url = "http://example.com"
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	var pageInformation webAnalyzer.PageInformation = webAnalyzer.GetPageInformation(resp.Body)
+
+	fmt.Println(pageInformation)
 }
