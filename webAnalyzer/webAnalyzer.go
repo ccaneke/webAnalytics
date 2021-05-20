aaf319b44a784900318dbe3d1ec70b10308307c7
Chukwuemezie Christopher Aneke <chukwuemezie.aneke.15@ucl.ac.uk>
2021-05-20 14:20:04 +0200

    add struct for web page contents to analyze

--- webAnalyzer/webAnalyzer.go
+++ webAnalyzer/webAnalyzer.go
@@ -1,1 +1,14 @@
 package webAnalyzer
+
+type WebPageContent struct {
+	HtmlVersion          string
+	PageTitle            string
+	NumH1                int
+	NumH2                int
+	NumH3                int
+	NumH4                int
+	NumAnchors           int
+	NumLinks             int
+	NumInaccessibleLinks int
+	LoginForm            bool
+}
